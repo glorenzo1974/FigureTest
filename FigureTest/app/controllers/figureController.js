29117caa6484cc358b062e2d48e069497ff0b8a3
glorenzo1974 <lorenzo.gerwin.dev@gmail.com>
2018-10-11 09:25:55 +0800

    Add project files.

---
+++ FigureTest/app/controllers/figureController.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('figureController', figureController);
+
+    figureController.$inject = [];
+
+    function figureController() {
+        var vm = this;
+
+        vm.side1 = "";
+        vm.side2 = "";
+        vm.side3 = "";
+        vm.side4 = "";
+        vm.checkResult = "";
+
+        function checkSquare(a, b, c, d) {
+            if (a == b && a == c && a == d) {
+                return true;
+            }
+            return false;
+        }
+
+        function checkRectangle(a, b, c, d) {
+            if (a == b && c == d) {
+                return true;
+            }
+            else if (a == d && c == b) {
+                return true;
+            }
+            else if (a == c && d == b) {
+                return true;
+            }
+            return false;
+        }
+
+        vm.clearInputs = function () {
+            vm.side1 = "";
+            vm.side2 = "";
+            vm.side3 = "";
+            vm.side4 = "";
+            vm.checkResult = "";
+        }
+
+        vm.checkFigure = function () {
+            if (vm.side1 == "" || vm.side2 == "" || vm.side3 == "" || vm.side4 == "") {
+                return;
+            }
+            var whatFigure = ""
+            if (checkSquare(vm.side1, vm.side2, vm.side3, vm.side4)) {
+                whatFigure = "This is a square."
+            }
+            else if (checkRectangle(vm.side1, vm.side2, vm.side3, vm.side4)) {
+                whatFigure = "This is a rectangle."
+            }
+            else {
+                whatFigure = "Sorry, this is neither a Square nor a Rectangle."
+            }
+            vm.checkResult = whatFigure;
+        }
+
+    }
+
+
+})();
